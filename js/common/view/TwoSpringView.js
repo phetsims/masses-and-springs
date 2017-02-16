3c14de3d92811c2bcce01bfd0e5d3ef8834520e5
Denz1994 <denz1994@gmail.com>
2017-02-16 13:43:19 -0700

    Moved scene switching logic to IntroScreenView and removed it from the TwoScreenCommonView

--- js/common/view/TwoSpringView.js
+++ js/common/view/TwoSpringView.js
@@ -1,7 +1,7 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
- * Common view mode for a screen using two masses.
+ * Common ScreenView for  using two masses.
  *
  * @author Matt Pennington
  * @author Denzell Barnett
@@ -15,13 +15,11 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var AquaRadioButton = require( 'SUN/AquaRadioButton' );
   var Bounds2 = require( 'DOT/Bounds2' );
-  var ConstantsControlPanel = require( 'MASSES_AND_SPRINGS/common/view/ConstantsControlPanel' );
   // var Util = require( 'DOT/Util' );
   var GravityControlPanel = require( 'MASSES_AND_SPRINGS/common/view/GravityControlPanel' );
   var DraggableRulerNode = require( 'MASSES_AND_SPRINGS/common/view/DraggableRulerNode' );
   var DraggableTimerNode = require( 'MASSES_AND_SPRINGS/common/view/DraggableTimerNode' );
   var EquilibriumLineNode = require( 'MASSES_AND_SPRINGS/common/view/EquilibriumLineNode' );
-  var Image = require( 'SCENERY/nodes/Image' );
   // var HBox = require( 'SCENERY/nodes/HBox' );
   var IndicatorVisibilityControlPanel = require( 'MASSES_AND_SPRINGS/common/view/IndicatorVisibilityControlPanel' );
   var MASPlayPauseStepControl = require( 'MASSES_AND_SPRINGS/common/view/MASPlayPauseStepControl' );
@@ -33,14 +31,11 @@ define( function( require ) {
   var Node = require( 'SCENERY/nodes/Node' );
   var OscillatingSpringNode = require( 'MASSES_AND_SPRINGS/common/view/OscillatingSpringNode' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var RadioButtonGroup = require( 'SUN/buttons/RadioButtonGroup' );
-  var RangeWithValue = require( 'DOT/RangeWithValue' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
   // var Spring = require( 'MASSES_AND_SPRINGS/common/model/Spring' );
   var SpringHangerNode = require( 'MASSES_AND_SPRINGS/common/view/SpringHangerNode' );
   var SpringConstantControlPanel = require( 'MASSES_AND_SPRINGS/common/view/SpringConstantControlPanel' );
-  var SpringLengthControlPanel = require( 'MASSES_AND_SPRINGS/common/view/SpringLengthControlPanel' );
   var SpringStopperButtonNode = require( 'MASSES_AND_SPRINGS/common/view/SpringStopperButtonNode' );
   var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
@@ -49,18 +44,12 @@ define( function( require ) {
   var Vector2 = require( 'DOT/Vector2' );
 
   // strings
-  var constantString = require( 'string!MASSES_AND_SPRINGS/constant' );
   var normalString = require( 'string!MASSES_AND_SPRINGS/normal' );
   var springConstantString = require( 'string!MASSES_AND_SPRINGS/springConstant' );
   var slowMotionString = require( 'string!MASSES_AND_SPRINGS/slowMotion' );
 
-  // images
-  var differentLengthIcon = require( 'image!MASSES_AND_SPRINGS/different-length-scene.png' );
-  var sameLengthIcon = require( 'image!MASSES_AND_SPRINGS/same-length-scene.png' );
-
   // constants
   var MAX_TEXT_WIDTH = 80;
-  var IMAGE_SCALE = .3;
 
   /**
    * TODO::: Remove mvt transforms from view objects
@@ -88,7 +77,7 @@ define( function( require ) {
     this.mvt = mvt; // Make mvt available to descendant types.
 
     //Spacing for top margin of layout bounds
-    var topSpacing = mvt.modelToViewY( model.ceilingY );
+    this.topSpacing = mvt.modelToViewY( model.ceilingY );
 
     // Add masses
     this.massLayer = new Node();
@@ -101,86 +90,41 @@ define( function( require ) {
     } );
 
     //  TODO: put in a vbox?? hmm... wrong place for this comment??
-    var firstOscillatingSpringNode = new OscillatingSpringNode( model.springs[ 0 ], mvtSpringHeight );
-    var secondOscillatingSpringNode = new OscillatingSpringNode( model.springs[ 1 ], mvtSpringHeight );
+    this.firstOscillatingSpringNode = new OscillatingSpringNode( model.springs[ 0 ], mvtSpringHeight );
+    this.secondOscillatingSpringNode = new OscillatingSpringNode( model.springs[ 1 ], mvtSpringHeight );
     // model.springs[ 0 ].naturalRestingLengthProperty.link(function (value){
     //   firstOscillatingSpringNode.loopsProperty.set(Util.roundSymmetric(value*24));
     //   console.log( 'model.springs[0].naturalRestingLengthProperty.get() = ' + model.springs[ 0 ].naturalRestingLengthProperty.get() );
     // });
-    this.addChild( firstOscillatingSpringNode );
-    this.addChild( secondOscillatingSpringNode );
+    this.addChild( this.firstOscillatingSpringNode );
+    this.addChild( this.secondOscillatingSpringNode );
 
     // Spring Hanger Node
-    var springHangerNode = new SpringHangerNode( model, mvt );
-    this.addChild( springHangerNode );
+    this.springHangerNode = new SpringHangerNode( model, mvt );
+    this.addChild( this.springHangerNode );
 
     // Spring Constant Control Panels
-    var firstSpringConstantControlPanel = new SpringConstantControlPanel(
+    this.firstSpringConstantControlPanel = new SpringConstantControlPanel(
       model.springs[ 0 ].springConstantProperty,
       model.springs[ 0 ].springConstantRange,
       StringUtils.format( springConstantString, 1 ),
       {
-        right: springHangerNode.springHangerNode.left - 40,
-        top: topSpacing,
+        right: this.springHangerNode.springHangerNode.left - 40,
+        top: this.topSpacing,
         maxWidth: 125
       } );
-    this.addChild( firstSpringConstantControlPanel );
+    this.addChild( this.firstSpringConstantControlPanel );
 
-    var secondSpringConstantControlPanel = new SpringConstantControlPanel(
+    this.secondSpringConstantControlPanel = new SpringConstantControlPanel(
       model.springs[ 1 ].springConstantProperty,
       model.springs[ 1 ].springConstantRange,
       StringUtils.format( springConstantString, 2 ),
       {
-        left: springHangerNode.springHangerNode.right + 40,
-        top: topSpacing,
+        left: this.springHangerNode.springHangerNode.right + 40,
+        top: this.topSpacing,
         maxWidth: 125
       } );
-    this.addChild( secondSpringConstantControlPanel );
-
-    // Spring Constant Length Control Panel
-    var springLengthControlPanel = new SpringLengthControlPanel(
-      model.springs[ 0 ].naturalRestingLengthProperty,
-      new RangeWithValue( .1, .5, .3 ),
-      StringUtils.format( 'Length 1', 1 ),
-      {
-        right: springHangerNode.springHangerNode.left - 40,
-        top: topSpacing,
-        maxWidth: 125
-      } );
-    this.addChild( springLengthControlPanel );
-
-    // @private panel that keeps thickness/spring constant at constant value
-    var constantsControlPanel = new ConstantsControlPanel(
-      firstOscillatingSpringNode.lineWidthProperty,
-      model.springs[ 0 ].springConstantProperty,
-      constantString,
-      {
-        minWidth: firstSpringConstantControlPanel.maxWidth,
-        left: firstSpringConstantControlPanel.left,
-        top: firstSpringConstantControlPanel.bottom + topSpacing
-      }
-    );
-    this.addChild( constantsControlPanel );
-
-    // Link that is responsible for switching the scenes
-    model.springLengthModeProperty.link( function( mode ) {
-      springLengthControlPanel.visible = (mode === 'adjustable-length');
-      constantsControlPanel.visible = springLengthControlPanel.visible;
-      firstSpringConstantControlPanel.visible = !springLengthControlPanel.visible;
-      secondSpringConstantControlPanel.visible = !springLengthControlPanel.visible;
-      model.springs[ 0 ].naturalRestingLengthProperty.link( function( lineLength ) {
-        // var lineWidth = options.minLineWidth + options.deltaLineWidth * ( springConstant - spring.springConstantRange.min ) / 2;
-        // self.lineWidthProperty.set( lineWidth );
-        firstOscillatingSpringNode.lineWidthProperty.set( 8 * lineLength );
-
-      } );
-      // Reset springs when scenes are switched
-      if ( mode === 'same-length' ) {
-        model.springs[ 0 ].reset();
-        model.springs[ 1 ].reset();
-      }
-    } );
-
+    this.addChild( this.secondSpringConstantControlPanel );
 
     // @public Initializes movable line
     this.movableLine = new MovableLineNode(
@@ -219,14 +163,14 @@ define( function( require ) {
 
     // Control Panel for display elements with varying visibility
     var indicatorVisibilityControlPanel = new IndicatorVisibilityControlPanel( model, {
-      top: topSpacing,
-      left: secondSpringConstantControlPanel.right + 10,
+      top: this.topSpacing,
+      left: this.secondSpringConstantControlPanel.right + 10,
       maxWidth: 180
     } );
     this.addChild( indicatorVisibilityControlPanel );
 
     // Gravity Control Panel
-    var gravityControlPanel = new GravityControlPanel(
+    this.gravityControlPanel = new GravityControlPanel(
       model.gravityProperty,
       model.gravityRange,
       model.bodies,
@@ -238,39 +182,39 @@ define( function( require ) {
         maxWidth: 180
       }
     );
-    this.addChild( gravityControlPanel );
+    this.addChild( this.gravityControlPanel );
 
     // Timer and Ruler
     var timerNode = new DraggableTimerNode(
       this.layoutBounds,
-      new Vector2( this.layoutBounds.left + 80, topSpacing + 35 ),
+      new Vector2( this.layoutBounds.left + 80, this.topSpacing + 35 ),
       model.timerSecondProperty,
       model.timerRunningProperty,
       model.timerVisibleProperty
     );
     var rulerNode = new DraggableRulerNode(
       this.layoutBounds,
-      new Vector2( this.layoutBounds.left + 50, topSpacing + 35 ),
+      new Vector2( this.layoutBounds.left + 50, this.topSpacing + 35 ),
       model.rulerVisibleProperty
     );
 
     // Toolbox Panel
-    var toolboxPanel = new ToolboxPanel( this.layoutBounds, rulerNode, timerNode, model.rulerVisibleProperty, model.timerVisibleProperty, {
-      top: gravityControlPanel.bottom + MassesAndSpringsConstants.PANEL_VERTICAL_SPACING,
-      left: gravityControlPanel.left,
-      minWidth: gravityControlPanel.width,
+    this.toolboxPanel = new ToolboxPanel( this.layoutBounds, rulerNode, timerNode, model.rulerVisibleProperty, model.timerVisibleProperty, {
+      top: this.gravityControlPanel.bottom + MassesAndSpringsConstants.PANEL_VERTICAL_SPACING,
+      left: this.gravityControlPanel.left,
+      minWidth: this.gravityControlPanel.width,
       maxWidth: 180
     } );
-    this.addChild( toolboxPanel );
+    this.addChild( this.toolboxPanel );
     // Done to for movableDragHandler handling intersecting bounds of panel and ruler
-    rulerNode.toolbox = toolboxPanel;
-    timerNode.toolbox = toolboxPanel;
+    rulerNode.toolbox = this.toolboxPanel;
+    timerNode.toolbox = this.toolboxPanel;
 
     // Reset All button
     var resetAllButton = new ResetAllButton( {
       listener: function() {
         model.reset();
-        gravityControlPanel.reset();
+        this.gravityControlPanel.reset();
         massNodes.forEach( function( massNode ) {
           massNode.moveToFront();
         } );
@@ -311,47 +255,17 @@ define( function( require ) {
 
     var firstSpringStopperButtonNode = new SpringStopperButtonNode( {
       listener: model.stopSpring.bind( model, 0 ),
-        right: springHangerNode.springHangerNode.left - 5,
-        top: topSpacing
+      right: this.springHangerNode.springHangerNode.left - 5,
+      top: this.topSpacing
       }
     );
     var secondSpringStopperButtonNode = new SpringStopperButtonNode( {
       listener: model.stopSpring.bind( model, 1 ),
-        left: springHangerNode.springHangerNode.right + 5,
-        top: topSpacing
+      left: this.springHangerNode.springHangerNode.right + 5,
+      top: this.topSpacing
       }
     );
 
-    // var springOne = new Spring ()
-    // sameLengthIcon = new HBox({
-    //   children:[new OscillatingSpringNode(,mvt),new OscillatingSpringNode(model.springs[1],mvt)],
-    //   pickable:false,
-    //   scale:IMAGE_SCALE
-    // });
-
-    //TODO: Create Icon node programmatically
-    var toggleButtonsContent = [ {
-      value: 'same-length',
-      node: new Image( sameLengthIcon, { scale: IMAGE_SCALE } )
-    }, {
-      value: 'adjustable-length',
-      node: new Image( differentLengthIcon, { scale: IMAGE_SCALE } )
-    } ];
-
-    var radioButtonGroup = new RadioButtonGroup( model.springLengthModeProperty, toggleButtonsContent, {
-      buttonContentXMargin: 4,
-      buttonContentYMargin: 4,
-      top: toolboxPanel.bottom + 55,
-      right: gravityControlPanel.right,
-      baseColor: 'black',
-      selectedStroke: 'yellow',
-      deselectedStroke: 'yellow',
-      selectedLineWidth: 1.3,
-      deselectedLineWidth: 0.6,
-      orientation: 'horizontal',
-      spacing: 13
-    } );
-    this.addChild( radioButtonGroup );
     this.addChild( firstSpringStopperButtonNode );
     this.addChild( secondSpringStopperButtonNode );
 
--- js/intro/view/IntroScreenView.js
+++ js/intro/view/IntroScreenView.js
@@ -1,7 +1,8 @@
-// Copyright 2016, University of Colorado Boulder
+// Copyright 2017, University of Colorado Boulder
 
 /**
- *
+ * ScreenView for the 'Intro' screen
+ * 
  * @author Matt Pennington
  * @author Denzell Barnett
  */
@@ -9,19 +10,104 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var ConstantsControlPanel = require( 'MASSES_AND_SPRINGS/common/view/ConstantsControlPanel' );
   var massesAndSprings = require( 'MASSES_AND_SPRINGS/massesAndSprings' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var RadioButtonGroup = require( 'SUN/buttons/RadioButtonGroup' );
+  var RangeWithValue = require( 'DOT/RangeWithValue' );
+  var SpringLengthControlPanel = require( 'MASSES_AND_SPRINGS/common/view/SpringLengthControlPanel' );
+  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var TwoSpringView = require( 'MASSES_AND_SPRINGS/common/view/TwoSpringView' );
+
+  // constants
+  var IMAGE_SCALE = .3;
+
+  // strings
+  var constantString = require( 'string!MASSES_AND_SPRINGS/constant' );
+
+  // images
+  var differentLengthIcon = require( 'image!MASSES_AND_SPRINGS/different-length-scene.png' );
+  var sameLengthIcon = require( 'image!MASSES_AND_SPRINGS/same-length-scene.png' );
 
   /**
    * @param {MassesAndSpringsModel} model
    * @constructor
    */
   function IntroScreenView( model ) {
+
+    var self = this;
+
     // Calls common two spring view
     TwoSpringView.call( this, model );
 
-    // Unique attributes of screen added here...
+    // Spring Constant Length Control Panel
+    this.springLengthControlPanel = new SpringLengthControlPanel(
+      model.springs[ 0 ].naturalRestingLengthProperty,
+      new RangeWithValue( .1, .5, .3 ),
+      StringUtils.format( 'Length 1', 1 ),
+      {
+        right: this.springHangerNode.springHangerNode.left - 40,
+        top: this.topSpacing,
+        maxWidth: 125
+      } );
+    this.addChild( this.springLengthControlPanel );
+
+    // @private panel that keeps thickness/spring constant at constant value
+    this.constantsControlPanel = new ConstantsControlPanel(
+      this.firstOscillatingSpringNode.lineWidthProperty,
+      model.springs[ 0 ].springConstantProperty,
+      constantString,
+      {
+        minWidth: this.firstSpringConstantControlPanel.maxWidth,
+        left: this.firstSpringConstantControlPanel.left,
+        top: this.firstSpringConstantControlPanel.bottom + this.topSpacing
+      }
+    );
+    this.addChild( this.constantsControlPanel );
+
+    // Link that is responsible for switching the scenes
+    model.springLengthModeProperty.link( function( mode ) {
+      self.springLengthControlPanel.visible = (mode === 'adjustable-length');
+      self.constantsControlPanel.visible = self.springLengthControlPanel.visible;
+      self.firstSpringConstantControlPanel.visible = !self.springLengthControlPanel.visible;
+      self.secondSpringConstantControlPanel.visible = !self.springLengthControlPanel.visible;
+      model.springs[ 0 ].naturalRestingLengthProperty.link( function( lineLength ) {
+        // var lineWidth = options.minLineWidth + options.deltaLineWidth * ( springConstant - spring.springConstantRange.min ) / 2;
+        // self.lineWidthProperty.set( lineWidth );
+        self.firstOscillatingSpringNode.lineWidthProperty.set( 8 * lineLength );
+
+      } );
+      // Reset springs when scenes are switched
+      if ( mode === 'same-length' ) {
+        model.springs[ 0 ].reset();
+        model.springs[ 1 ].reset();
+      }
+    } );
+
+    //TODO: Create Icon node programmatically
+    var toggleButtonsContent = [ {
+      value: 'same-length',
+      node: new Image( sameLengthIcon, { scale: IMAGE_SCALE } )
+    }, {
+      value: 'adjustable-length',
+      node: new Image( differentLengthIcon, { scale: IMAGE_SCALE } )
+    } ];
+
+    var sceneRadioButtonGroup = new RadioButtonGroup( model.springLengthModeProperty, toggleButtonsContent, {
+      buttonContentXMargin: 4,
+      buttonContentYMargin: 4,
+      top: this.toolboxPanel.bottom + 55,
+      right: this.gravityControlPanel.right,
+      baseColor: 'black',
+      selectedStroke: 'yellow',
+      deselectedStroke: 'yellow',
+      selectedLineWidth: 1.3,
+      deselectedLineWidth: 0.6,
+      orientation: 'horizontal',
+      spacing: 13
+    } );
+    this.addChild( sceneRadioButtonGroup );
   }
 
   massesAndSprings.register( 'IntroScreenView', IntroScreenView );
