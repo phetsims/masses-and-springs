490f51ba909b08914f101c1d08a163f5cc384dfe
Denz1994 <denz1994@gmail.com>
2017-03-16 13:55:32 -0600

    Insert require statement to correct last commit.

--- js/energy/EnergyScreen.js
+++ js/energy/EnergyScreen.js
@@ -14,6 +14,9 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Screen = require( 'JOIST/Screen' );
   var massesAndSprings = require( 'MASSES_AND_SPRINGS/massesAndSprings' );
+  var Property = require( 'AXON/Property' );
+  var Color = require( 'SCENERY/util/Color' );
+  var TColor = require( 'SCENERY/util/TColor' );
 
   // strings
   var energyString = require( 'string!MASSES_AND_SPRINGS/energy' );
