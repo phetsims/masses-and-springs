7d66fa3e7a02c2545dd3412ae56b3bb7f39a05d5
Denz1994 <denz1994@gmail.com>
2017-03-20 10:12:29 -0600

    Refactoring objects in common view. This was a result of addressing a previous TODO. Fixed #54.

--- js/common/view/TwoSpringView.js
+++ js/common/view/TwoSpringView.js
@@ -321,11 +321,11 @@ define( function( require ) {
     this.addChild( secondSpringStopperButtonNode )
     ;
     //Reference lines from indicator visibility box
-    this.addChild( this.firstSpringEquilibriumLine );
-    this.addChild( this.secondSpringEquilibriumLine );
-    this.addChild( this.firstNaturalLengthLine );
-    this.addChild( this.secondNaturalLengthLine );
-    this.addChild( this.movableLine );
+    this.addChild( this.firstSpringEquilibriumLineNode );
+    this.addChild( this.secondSpringEquilibriumLineNode );
+    this.addChild( this.firstNaturalLengthLineNode );
+    this.addChild( this.secondNaturalLengthLineNode );
+    this.addChild( this.movableLineNode );
     this.addChild( this.massLayer );
 
     // Adding Nodes in tool box
