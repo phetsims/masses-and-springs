fe4262fe15531746eb649c35abaf4814496c38c5
Denz1994 <denz1994@gmail.com>
2017-05-04 17:16:52 -0600

    MassValueControlPanel options added to reflect design doc

--- js/common/view/MassValueControlPanel.js
+++ js/common/view/MassValueControlPanel.js
@@ -17,10 +17,10 @@ define( function( require ) {
   var Panel = require( 'SUN/Panel' );
   var Property = require( 'AXON/Property' );
   var Range = require( 'DOT/Range' );
-  // var Text = require( 'SCENERY/nodes/Text' );
+  var Text = require( 'SCENERY/nodes/Text' );
 
   // strings
-  // var massString = require( 'string!MASSES_AND_SPRINGS/mass' );
+  var massString = require( 'string!MASSES_AND_SPRINGS/mass' );
 
   /**
    * @param {Tandem} tandem
@@ -28,19 +28,20 @@ define( function( require ) {
    * @constructor
    */
   function MassValueControlPanel( mass, tandem ) {
-    // var title = new Text( massString, { font: MassesAndSpringsConstants.TITLE_FONT } );
     var massProperty = new Property( mass.mass );
 
     massProperty.link( function( massValue ) {
-      mass.mass = massValue/1000;
-      console.log(massValue);
+      mass.mass = massValue / 1000;
+      console.log( massValue );
     } );
 
-    Panel.call( this, new NumberControl( 'mass', massProperty, new Range( 50, 300 ), {
+    Panel.call( this, new NumberControl( massString, massProperty, new Range( 50, 300 ), {
       valuePattern: '{0} g',
       majorTickLength: 10,
       trackSize: new Dimension2( 100, 2 ),
-      thumbSize: new Dimension2( 13, 22 )
+      thumbSize: new Dimension2( 13, 22 ),
+      titleFont: MassesAndSpringsConstants.TITLE_FONT,
+      arrowButtonScale: .6
     } ), {
       fill: 'rgb( 240, 240, 240 )',
       cornerRadius: MassesAndSpringsConstants.PANEL_CORNER_RADIUS
