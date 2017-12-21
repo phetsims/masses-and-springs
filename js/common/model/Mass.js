efc0b97765c1aa833185083eb5e58161c7ea63b0
denz1994 <denz1994@gmail.com>
2017-12-20 23:29:37 -0500

    Labels added for common energy graph along with alignment correction for barNodes.

--- js/common/model/Mass.js
+++ js/common/model/Mass.js
@@ -204,6 +204,7 @@ define( function( require ) {
     // @public {Property.<number>} Thermal energy of the mass
     this.thermalEnergyProperty = new DerivedProperty( [ this.initialTotalEnergyProperty, this.totalEnergyProperty ],
       function( initialEnergy, totalEnergy ) {
+        return initialEnergy - totalEnergy;
       } );
 
 
