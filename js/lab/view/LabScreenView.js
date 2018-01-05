33d9bf4476f67296f177d393bf00d0fd329af06c
denz1994 <denz1994@gmail.com>
2018-01-05 15:10:47 -0500

    A consolidated options panel was added for the lab screen. #181

--- js/lab/view/LabScreenView.js
+++ js/lab/view/LabScreenView.js
@@ -11,10 +11,13 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var GravityAndDampingControlPanel = require( 'MASSES_AND_SPRINGS/common/view/GravityAndDampingControlPanel' );
   var massesAndSprings = require( 'MASSES_AND_SPRINGS/massesAndSprings' );
   var MassesAndSpringsConstants = require( 'MASSES_AND_SPRINGS/common/MassesAndSpringsConstants' );
   var OneSpringView = require( 'MASSES_AND_SPRINGS/common/view/OneSpringView' );
   var VectorVisibilityControlPanel = require( 'MASSES_AND_SPRINGS/vectors/view/VectorVisibilityControlPanel' );
+  var Panel = require( 'SUN/Panel' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
 
   /**
    * @param {MassesAndSpringsModel} model
@@ -36,18 +39,57 @@ define( function( require ) {
       model,
       tandem.createTandem( 'vectorVisibilityControlPanel' ),
       {
-        top: this.toolboxPanel.bottom + 55,
-        left: this.gravityAndDampingControlPanel.left,
         maxWidth: MassesAndSpringsConstants.PANEL_MAX_WIDTH,
+        xMargin: 0,
+        yMargin: 0,
+        stroke: null,
         showForces: false
       }
     );
-    this.addChild( vectorVisibilityControlPanel );
-    vectorVisibilityControlPanel.moveToBack();
+
+    // Gravity Control Panel
+    var gravityAndDampingControlPanel = new GravityAndDampingControlPanel(
+      model, this, tandem.createTandem( 'gravityAndDampingControlPanel' ), {
+        minWidth: 1,
+        maxWidth: MassesAndSpringsConstants.PANEL_MAX_WIDTH,
+        dampingVisible: false,
+        xMargin: 0,
+        yMargin: 0,
+        stroke: null
+      } );
+
+    // VBox that contains all of the panel's content
+    var optionsVBox = new VBox( {
+      spacing: 10,
+      children: [
+        this.referenceLinePanel,
+        MassesAndSpringsConstants.LINE_SEPARATOR(),
+        gravityAndDampingControlPanel,
+        MassesAndSpringsConstants.LINE_SEPARATOR(),
+        vectorVisibilityControlPanel,
+        MassesAndSpringsConstants.LINE_SEPARATOR(),
+        this.toolboxPanel
+      ]
+    } );
+
+    // Panel that will display all the toggleable options.
+    var optionsPanel = new Panel(
+      optionsVBox,
+      {
+        xMargin: 10,
+        fill: MassesAndSpringsConstants.PANEL_FILL,
+        cornerRadius: MassesAndSpringsConstants.PANEL_CORNER_RADIUS,
+        tandem: tandem.createTandem( 'ReferenceLinePanel' ),
+        minWidth: MassesAndSpringsConstants.PANEL_MIN_WIDTH
+      } );
+
+    this.addChild( optionsPanel );
+    optionsPanel.moveToBack();
 
     // Adjust the floating panels to the visibleBounds of the screen.
     this.visibleBoundsProperty.link( function( visibleBounds ) {
-      vectorVisibilityControlPanel.right = visibleBounds.right - self.spacing;
+      optionsPanel.top = visibleBounds.top + self.spacing;
+      optionsPanel.right = visibleBounds.right - self.spacing;
     } );
     this.gravityAndDampingControlPanel.options.hSliderTweakersEnabled = 0.5;
 
