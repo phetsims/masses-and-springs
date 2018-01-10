408b4dc662c413af36808d2acecba13e6da6eb94
denz1994 <denz1994@gmail.com>
2018-01-10 10:26:43 -0500

    Spring position adjustments. #186.

--- js/common/model/MassesAndSpringsModel.js
+++ js/common/model/MassesAndSpringsModel.js
@@ -194,7 +194,7 @@ define( function( require ) {
      */
     createSpring: function( x, tandem ) {
       var spring = new Spring(
-        new Vector2( x, MassesAndSpringsConstants.CEILING_Y - 0.03 ),
+        new Vector2( x, MassesAndSpringsConstants.CEILING_Y - 0.01 ),
         MassesAndSpringsConstants.DEFAULT_SPRING_LENGTH,
         this.dampingProperty.get(),
         tandem
