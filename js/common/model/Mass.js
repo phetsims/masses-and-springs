5135585cdca9cb6485118bd9a2bf1e48d80a5b03
denz1994 <denz1994@gmail.com>
2018-03-20 10:22:18 -0400

    Lint fix. #253

--- js/common/model/Mass.js
+++ js/common/model/Mass.js
@@ -24,7 +24,6 @@ define( function( require ) {
   var Property = require( 'AXON/Property' );
   var PropertyIO = require( 'AXON/PropertyIO' );
   var Range = require( 'DOT/Range' );
-  var RangeWithValue = require( 'DOT/RangeWithValue' );
   var Util = require( 'DOT/Util' );
   var Vector2 = require( 'DOT/Vector2' );
 
