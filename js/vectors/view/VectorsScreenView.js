f4a30366fc496763d3627a534dfdce637b5ec7c4
denz1994 <denz1994@gmail.com>
2018-03-29 10:41:57 -0400

    Code simplification for displacementArrowNode visiblility. #253

--- js/vectors/view/VectorsScreenView.js
+++ js/vectors/view/VectorsScreenView.js
@@ -64,23 +64,11 @@ define( function( require ) {
     // Equilibrium of mass is dependent on the mass being attached and the visibility of the equilibrium line.
     var firstMassEquilibriumVisibilityProperty = new DerivedProperty( [ model.equilibriumPositionVisibleProperty, model.firstSpring.massAttachedProperty ],
       function( equilibriumPositionVisible, massAttached ) {
-        //REVIEW: return massAttached && equilibriumPositionVisible;
-        if ( massAttached ) {
-          return equilibriumPositionVisible;
-        }
-        else {
-          return false;
-        }
+        return massAttached && equilibriumPositionVisible;
       } );
     var secondMassEquilibriumVisibilityProperty = new DerivedProperty( [ model.equilibriumPositionVisibleProperty, model.secondSpring.massAttachedProperty ],
       function( equilibriumPositionVisible, massAttached ) {
-        //REVIEW: return massAttached && equilibriumPositionVisible;
-        if ( massAttached ) {
-          return equilibriumPositionVisible;
-        }
-        else {
-          return false;
-        }
+        return massAttached && equilibriumPositionVisible;
       } );
     //REVIEW: Some duplication for things for each spring. Is there an easy/moderate way to remove this? If not no worries
 
