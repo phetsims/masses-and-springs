1cd514caf41224f54a35497378eaf4787dc958cf
denz1994 <denz1994@gmail.com>
2018-04-03 10:08:48 -0400

    Removing stale TODO. #253

--- js/lab/model/LabModel.js
+++ js/lab/model/LabModel.js
@@ -26,8 +26,6 @@ define( function( require ) {
    * @constructor
    */
   function LabModel( tandem ) {
-    // TODO: Think about creating mass objects above, and then passing into the constructor (instead of having to function-call to create, and then index into to modify)
-    //REVIEW: Handle TODO
 
     MassesAndSpringsModel.call( this, tandem );
 
