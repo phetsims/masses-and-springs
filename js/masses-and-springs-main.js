45ef41803366cb624aecc3c504617f89d8e2ff9a
denz1994 <denz1994@gmail.com>
2018-05-16 09:20:19 -0400

    Removing TODO. Updating credits will be tracked in #263.

--- js/masses-and-springs-main.js
+++ js/masses-and-springs-main.js
@@ -26,8 +26,6 @@ define( function( require ) {
 
   var simOptions = {
     credits: {
-      //REVIEW: I recommend making an issue about credits. Then you can't forget before deploying!
-      //TODO fill in proper credits, all of these fields are optional, see joist.AboutDialog
       leadDesign: 'Amy Rouinfar',
       softwareDevelopment: 'Denzell Barnett, Matt Pennington',
       team: 'Ariel Paul, Kathy Perkins, Mike Dubson',
