538875c120f83aff40639c801b171d4d47e00d05
denz1994 <denz1994@gmail.com>
2018-05-22 11:16:38 -0400

    The periodTrace is only drawn when it is visible. #273.

--- js/lab/view/PeriodTraceNode.js
+++ js/lab/view/PeriodTraceNode.js
@@ -59,59 +59,65 @@ define( function( require ) {
   massesAndSprings.register( 'PeriodTraceNode', PeriodTraceNode );
 
   return inherit( Node, PeriodTraceNode, {
-    step: function( dt , playingProperty) {
-      if ( this.periodTrace.stateProperty.value === 4 && playingProperty.value ) {
-        this.colorAlpha = Math.max( 0, this.colorAlpha - FADE_OUT_SPEED * dt );
-        this.traceColor.alpha = this.colorAlpha;
 
-        if ( this.colorAlpha === 0 ) {
-          this.periodTrace.onFaded();
-          this.traceColor.alpha = 1;
-          this.colorAlpha = 1;
-        }
-      }
-
+    /**
+     *
+     * @param {number} dt
+     * @param {Property.<boolean>} playingProperty: whether the sim is playing or not
+     */
+    step: function( dt, playingProperty ) {
       var spring = this.periodTrace.springProperty.value;
-      var modelViewTransform = this.modelViewTransform;
-      var mass = spring.massAttachedProperty.value;
-      if ( mass && !mass.userControlledProperty.value ) {
+      if ( spring.periodTraceVisibilityProperty.value ) {
+        if ( this.periodTrace.stateProperty.value === 4 && playingProperty.value ) {
+          this.colorAlpha = Math.max( 0, this.colorAlpha - FADE_OUT_SPEED * dt );
+          this.traceColor.alpha = this.colorAlpha;
 
-        var equilibriumYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value );
-        var firstPeakYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value + this.periodTrace.firstPeakY );
-        var secondPeakYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value + this.periodTrace.secondPeakY );
-        var currentYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value + spring.massEquilibriumDisplacementProperty.value );
-
-        var state = this.periodTrace.stateProperty.value; // 0 to 4
-        if ( state === 0 ) {
-          this.visible = false && spring.periodTraceVisibilityProperty.value;
-        }
-        else {
-          this.visible = spring.periodTraceVisibilityProperty.value;
-          var shape = new Shape();
-          shape.moveTo( this.originalX, equilibriumYPosition ); // sets our current position
-
-          // draws a line from our current position to a NEW position, then sets our current position to the NEW position
-          shape.lineTo( this.originalX, state === 1 ? currentYPosition : firstPeakYPosition );
-
-          if ( state > 1 ) {
-
-            // first connector
-            shape.lineTo( this.middleX, firstPeakYPosition );
-
-            // second line
-            shape.lineTo( this.middleX, state === 2 ? currentYPosition : secondPeakYPosition );
-            if ( state > 2 ) {
-
-              // second connector
-              shape.lineTo( this.lastX, secondPeakYPosition );
-
-              // third line
-              shape.lineTo( this.lastX, state === 3 ? currentYPosition : equilibriumYPosition );
-            }
+          if ( this.colorAlpha === 0 ) {
+            this.periodTrace.onFaded();
+            this.traceColor.alpha = 1;
+            this.colorAlpha = 1;
           }
-          this.path.shape = shape;
+        }
 
+        var modelViewTransform = this.modelViewTransform;
+        var mass = spring.massAttachedProperty.value;
+        if ( mass && !mass.userControlledProperty.value ) {
 
+          var equilibriumYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value );
+          var firstPeakYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value + this.periodTrace.firstPeakY );
+          var secondPeakYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value + this.periodTrace.secondPeakY );
+          var currentYPosition = modelViewTransform.modelToViewY( spring.massEquilibriumYPositionProperty.value + spring.massEquilibriumDisplacementProperty.value );
+
+          var state = this.periodTrace.stateProperty.value; // 0 to 4
+          if ( state === 0 ) {
+            this.visible = false && spring.periodTraceVisibilityProperty.value;
+          }
+          else {
+            this.visible = spring.periodTraceVisibilityProperty.value;
+            var shape = new Shape();
+            shape.moveTo( this.originalX, equilibriumYPosition ); // sets our current position
+
+            // draws a line from our current position to a NEW position, then sets our current position to the NEW position
+            shape.lineTo( this.originalX, state === 1 ? currentYPosition : firstPeakYPosition );
+
+            if ( state > 1 ) {
+
+              // first connector
+              shape.lineTo( this.middleX, firstPeakYPosition );
+
+              // second line
+              shape.lineTo( this.middleX, state === 2 ? currentYPosition : secondPeakYPosition );
+              if ( state > 2 ) {
+
+                // second connector
+                shape.lineTo( this.lastX, secondPeakYPosition );
+
+                // third line
+                shape.lineTo( this.lastX, state === 3 ? currentYPosition : equilibriumYPosition );
+              }
+            }
+            this.path.shape = shape;
+          }
         }
       }
       else {
