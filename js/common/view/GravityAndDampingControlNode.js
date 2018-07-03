1c4027834fc62760b3e3c6541e6ac6dc8aaa5a9d
denz1994 <denz1994@gmail.com>
2018-07-03 15:35:20 -0400

    Increase readout font size to same size at title. #292.

--- js/common/view/GravityAndDampingControlNode.js
+++ js/common/view/GravityAndDampingControlNode.js
@@ -123,7 +123,8 @@ define( function( require ) {
       } ),
       valuePattern: StringUtils.fillIn( gravityValueString, {
         gravity: '{0}'
-      }, { font: new PhetFont( { size: 14, weight: 'bold' } ) } ),
+      } ),
+      valueFont: new PhetFont( { size: 14} ),
       useRichText: true,
       decimalPlaces: 1,
       valueMaxWidth: MAX_WIDTH * 2,
