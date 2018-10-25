d850e309a30d43754911813ae4d0bf456eff5e0e
zepumph <michael.kauzmann@colorado.edu>
2018-10-25 13:02:43 -0800

    move phetioInherit to tandem, https://github.com/phetsims/phet-io/issues/1401

--- js/common/model/BodyIO.js
+++ js/common/model/BodyIO.js
@@ -11,10 +11,10 @@ define( function( require ) {
   // modules
   var massesAndSprings = require( 'MASSES_AND_SPRINGS/massesAndSprings' );
   var ObjectIO = require( 'TANDEM/types/ObjectIO' );
+  var phetioInherit = require( 'TANDEM/phetioInherit' );
 
   // ifphetio
   var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
-  var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
 
   /**
    * @param {Body} body
