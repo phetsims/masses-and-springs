93ba9b5786cc02bec191d70a545b2fd683041a9e
samreid <reids@colorado.edu>
2020-02-27 12:07:57 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/masses-and-springs-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import massesAndSprings from './massesAndSprings.js';
+
+const massesAndSpringsStrings = getStringModule( 'MASSES_AND_SPRINGS' );
+
+massesAndSprings.register( 'massesAndSpringsStrings', massesAndSpringsStrings );
+
+export default massesAndSpringsStrings;
