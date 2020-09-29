db47f268d881fa4d993896357bca5d1581c5873b
pixelzoom <cmalley@pixelzoom.com>
2020-09-29 17:31:53 -0600

    convert RectangularPushButton subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/StopperButtonNode.js
+++ js/common/view/StopperButtonNode.js
@@ -6,36 +6,36 @@
  * @author Denzell Barnett (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import StopSignNode from '../../../../scenery-phet/js/StopSignNode.js';
 import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
 import massesAndSprings from '../../massesAndSprings.js';
 import MassesAndSpringsConstants from '../MassesAndSpringsConstants.js';
 
-/**
- * Constructor for return button
- * @param {Tandem} tandem
- * @param {Object} [options]
- * @constructor
- */
-function StopperButtonNode( tandem, options ) {
-  options = merge( {
-    xMargin: 3,
-    yMargin: 3,
-    touchAreaXDilation: 6,
-    touchAreaYDilation: 6,
-    baseColor: MassesAndSpringsConstants.PANEL_FILL,
-    content: new StopSignNode( {
-      tandem: tandem.createTandem( 'stopSignNode' ),
-      scale: 0.33
-    } ),
-    tandem: tandem
-  }, options );
-  RectangularPushButton.call( this, options );
+class StopperButtonNode extends RectangularPushButton {
+
+  /**
+   * @param {Tandem} tandem
+   * @param {Object} [options]
+   */
+  constructor( tandem, options ) {
+
+    options = merge( {
+      xMargin: 3,
+      yMargin: 3,
+      touchAreaXDilation: 6,
+      touchAreaYDilation: 6,
+      baseColor: MassesAndSpringsConstants.PANEL_FILL,
+      content: new StopSignNode( {
+        tandem: tandem.createTandem( 'stopSignNode' ),
+        scale: 0.33
+      } ),
+      tandem: tandem
+    }, options );
+
+    super( options );
+  }
 }
 
 massesAndSprings.register( 'StopperButtonNode', StopperButtonNode );
-
-inherit( RectangularPushButton, StopperButtonNode );
 export default StopperButtonNode;
