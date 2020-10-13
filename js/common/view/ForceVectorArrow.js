954fe8c2ef9dac7683eeca7bb4724423ea49b3df
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 10:50:31 -0600

    convert to ArrowNode and its subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/ForceVectorArrow.js
+++ js/common/view/ForceVectorArrow.js
@@ -6,26 +6,25 @@
  * @author Denzell Barnett (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import massesAndSprings from '../../massesAndSprings.js';
 
-/**
- * @param {Color} color
- * @constructor
- */
-function ForceVectorArrow( color ) {
-  ArrowNode.call( this, 5, 0, 31, 0, {
-    fill: color,
-    stroke: color,
-    centerY: 0,
-    tailWidth: 3,
-    headWidth: 11,
-    pickable: false
-  } );
+class ForceVectorArrow extends ArrowNode {
+
+  /**
+   * @param {Color} color
+   */
+  constructor( color ) {
+    super( 5, 0, 31, 0, {
+      fill: color,
+      stroke: color,
+      centerY: 0,
+      tailWidth: 3,
+      headWidth: 11,
+      pickable: false
+    } );
+  }
 }
 
 massesAndSprings.register( 'ForceVectorArrow', ForceVectorArrow );
-
-inherit( ArrowNode, ForceVectorArrow );
 export default ForceVectorArrow;
--- js/common/view/VectorArrow.js
+++ js/common/view/VectorArrow.js
@@ -6,26 +6,25 @@
  * @author Denzell Barnett (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import massesAndSprings from '../../massesAndSprings.js';
 
-/**
- * @param {Color} color
- * @constructor
- */
-function VectorArrow( color ) {
-  ArrowNode.call( this, 10, 0, 34, 0, {
-    fill: color,
-    stroke: 'black',
-    centerY: 0,
-    tailWidth: 8,
-    headWidth: 14,
-    pickable: false
-  } );
+class VectorArrow extends ArrowNode {
+
+  /**
+   * @param {Color} color
+   */
+  constructor( color ) {
+    super( 10, 0, 34, 0, {
+      fill: color,
+      stroke: 'black',
+      centerY: 0,
+      tailWidth: 8,
+      headWidth: 14,
+      pickable: false
+    } );
+  }
 }
 
 massesAndSprings.register( 'VectorArrow', VectorArrow );
-
-inherit( ArrowNode, VectorArrow );
 export default VectorArrow;
