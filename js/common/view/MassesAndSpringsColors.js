c239cb095df63557dad21905ca5e9e325cdb383e
samreid <reids@colorado.edu>
2021-08-20 16:15:23 -0600

    Add Namespace parameter to ProfileColorProperty constructor, see https://github.com/phetsims/scenery/issues/1259

--- js/common/view/MassesAndSpringsColors.js
+++ js/common/view/MassesAndSpringsColors.js
@@ -16,43 +16,43 @@ import massesAndSprings from '../../massesAndSprings.js';
 // Initialize colors for each profile, by string key. If a basics color is not defined, it will take the
 // 'default' value provided.
 const MassesAndSpringsColors = {
-  backgroundProperty: new ProfileColorProperty( 'background', {
+  backgroundProperty: new ProfileColorProperty( massesAndSprings, 'background', {
     default: Color.white,
     basics: new Color( 'rgb( 255, 250, 227 )' )
   } ),
-  smallMysteryMassProperty: new ProfileColorProperty( 'smallMysteryMass', {
+  smallMysteryMassProperty: new ProfileColorProperty( massesAndSprings, 'smallMysteryMass', {
     default: new Color( 'rgb( 246, 164, 255 )' ),
     basics: new Color( 'rgb( 185, 0, 38 )' )
   } ),
-  mediumMysteryMassProperty: new ProfileColorProperty( 'mediumMysteryMass', {
+  mediumMysteryMassProperty: new ProfileColorProperty( massesAndSprings, 'mediumMysteryMass', {
     default: new Color( 'rgb( 0, 222, 224 )' ),
     basics: new Color( 'rgb( 0, 107, 161 )' )
   } ),
-  largeMysteryMassProperty: new ProfileColorProperty( 'largeMysteryMass', {
+  largeMysteryMassProperty: new ProfileColorProperty( massesAndSprings, 'largeMysteryMass', {
     default: new Color( 'rgb( 250, 186, 75)' ),
     basics: new Color( 'rgb( 0, 104, 55 )' )
   } ),
-  labeledMassProperty: new ProfileColorProperty( 'labeledMass', {
+  labeledMassProperty: new ProfileColorProperty( massesAndSprings, 'labeledMass', {
     default: new Color( 'rgb( 153, 153, 153 )' ),
     basics: new Color( 'rgb( 153, 153, 153 )' )
   } ),
-  adjustableMassProperty: new ProfileColorProperty( 'adjustableMass', {
+  adjustableMassProperty: new ProfileColorProperty( massesAndSprings, 'adjustableMass', {
     default: new Color( 'rgb( 247, 151, 34 )' ),
     basics: new Color( 'rgb( 247, 151, 34 )' )
   } ),
-  unstretchedLengthProperty: new ProfileColorProperty( 'unstretchedLength', {
+  unstretchedLengthProperty: new ProfileColorProperty( massesAndSprings, 'unstretchedLength', {
     default: new Color( 'rgb( 65, 66, 232 )' )
   } ),
-  restingPositionProperty: new ProfileColorProperty( 'restingPosition', {
+  restingPositionProperty: new ProfileColorProperty( massesAndSprings, 'restingPosition', {
     default: new Color( 'rgb( 0, 180, 0 )' )
   } ),
-  movableLineProperty: new ProfileColorProperty( 'movableLine', {
+  movableLineProperty: new ProfileColorProperty( massesAndSprings, 'movableLine', {
     default: new Color( 'rgb( 255, 0, 0 )' )
   } ),
-  velocityVectorProperty: new ProfileColorProperty( 'velocityVector', {
+  velocityVectorProperty: new ProfileColorProperty( massesAndSprings, 'velocityVector', {
     default: PhetColorScheme.VELOCITY
   } ),
-  accelerationVectorProperty: new ProfileColorProperty( 'accelerationVector', {
+  accelerationVectorProperty: new ProfileColorProperty( massesAndSprings, 'accelerationVector', {
     default: PhetColorScheme.ACCELERATION
   } )
 };
