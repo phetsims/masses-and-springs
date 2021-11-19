e20f0cdc5bde8931a56968973b397bb43842a31d
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:40 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/massesAndSpringsStrings.ts
@@ -0,0 +1,78 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import massesAndSprings from './massesAndSprings.js';
+
+type StringsType = {
+  'acceleration': string,
+  'body': {
+    'custom': string,
+    'earth': string,
+    'jupiter': string,
+    'moon': string,
+    'planetX': string
+  },
+  'cm': string,
+  'constantParameter': string,
+  'damping': string,
+  'dampingEqualsZero': string,
+  'displacement': string,
+  'elasticPotentialEnergy': string,
+  'energyGraph': string,
+  'energyLegend': string,
+  'equilibriumPosition': string,
+  'eTherm': string,
+  'eTot': string,
+  'forces': string,
+  'gravitationalPotentialEnergy': string,
+  'gravityValue': string,
+  'gravity': string,
+  'heightEqualsZero': string,
+  'ke': string,
+  'kineticEnergy': string,
+  'large': string,
+  'long': string,
+  'lots': string,
+  'mass': string,
+  'massEquilibrium': string,
+  'massValue': string,
+  'masses-and-springs': {
+    'title': string
+  },
+  'naturalLength': string,
+  'movableLine': string,
+  'netForce': string,
+  'none': string,
+  'peElas': string,
+  'peGrav': string,
+  'questionMark': string,
+  'screen': {
+    'energy': string,
+    'intro': string,
+    'lab': string,
+    'vectors': string
+  },
+  'short': string,
+  'spring': string,
+  'springStrength': string,
+  'springConstant': string,
+  'springConstantPattern': string,
+  'length': string,
+  'periodTrace': string,
+  'small': string,
+  'thermalEnergy': string,
+  'thickness': string,
+  'totalEnergy': string,
+  'velocity': string,
+  'whatIsTheValueOfGravity': string
+};
+
+const massesAndSpringsStrings = getStringModule( 'MASSES_AND_SPRINGS' ) as StringsType;
+
+massesAndSprings.register( 'massesAndSpringsStrings', massesAndSpringsStrings );
+
+export default massesAndSpringsStrings;
