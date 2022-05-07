4c6ad77b1b2632a48fb210f7ed413e8d709f7066
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:32 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/massesAndSpringsStrings.ts
+++ js/massesAndSpringsStrings.ts
@@ -8,67 +8,67 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import massesAndSprings from './massesAndSprings.js';
 
 type StringsType = {
-  'acceleration': string,
+  'acceleration': string;
   'body': {
-    'custom': string,
-    'earth': string,
-    'jupiter': string,
-    'moon': string,
-    'planetX': string
-  },
-  'cm': string,
-  'constantParameter': string,
-  'damping': string,
-  'dampingEqualsZero': string,
-  'displacement': string,
-  'elasticPotentialEnergy': string,
-  'energyGraph': string,
-  'energyLegend': string,
-  'equilibriumPosition': string,
-  'eTherm': string,
-  'eTot': string,
-  'forces': string,
-  'gravitationalPotentialEnergy': string,
-  'gravityValue': string,
-  'gravity': string,
-  'heightEqualsZero': string,
-  'ke': string,
-  'kineticEnergy': string,
-  'large': string,
-  'long': string,
-  'lots': string,
-  'mass': string,
-  'massEquilibrium': string,
-  'massValue': string,
+    'custom': string;
+    'earth': string;
+    'jupiter': string;
+    'moon': string;
+    'planetX': string;
+  };
+  'cm': string;
+  'constantParameter': string;
+  'damping': string;
+  'dampingEqualsZero': string;
+  'displacement': string;
+  'elasticPotentialEnergy': string;
+  'energyGraph': string;
+  'energyLegend': string;
+  'equilibriumPosition': string;
+  'eTherm': string;
+  'eTot': string;
+  'forces': string;
+  'gravitationalPotentialEnergy': string;
+  'gravityValue': string;
+  'gravity': string;
+  'heightEqualsZero': string;
+  'ke': string;
+  'kineticEnergy': string;
+  'large': string;
+  'long': string;
+  'lots': string;
+  'mass': string;
+  'massEquilibrium': string;
+  'massValue': string;
   'masses-and-springs': {
-    'title': string
-  },
-  'naturalLength': string,
-  'movableLine': string,
-  'netForce': string,
-  'none': string,
-  'peElas': string,
-  'peGrav': string,
-  'questionMark': string,
+    'title': string;
+  };
+  'naturalLength': string;
+  'movableLine': string;
+  'netForce': string;
+  'none': string;
+  'peElas': string;
+  'peGrav': string;
+  'questionMark': string;
   'screen': {
-    'energy': string,
-    'intro': string,
-    'lab': string,
-    'vectors': string
-  },
-  'short': string,
-  'spring': string,
-  'springStrength': string,
-  'springConstant': string,
-  'springConstantPattern': string,
-  'length': string,
-  'periodTrace': string,
-  'small': string,
-  'thermalEnergy': string,
-  'thickness': string,
-  'totalEnergy': string,
-  'velocity': string,
-  'whatIsTheValueOfGravity': string
+    'energy': string;
+    'intro': string;
+    'lab': string;
+    'vectors': string;
+  };
+  'short': string;
+  'spring': string;
+  'springStrength': string;
+  'springConstant': string;
+  'springConstantPattern': string;
+  'length': string;
+  'periodTrace': string;
+  'small': string;
+  'thermalEnergy': string;
+  'thickness': string;
+  'totalEnergy': string;
+  'velocity': string;
+  'whatIsTheValueOfGravity': string;
 };
 
 const massesAndSpringsStrings = getStringModule( 'MASSES_AND_SPRINGS' ) as StringsType;
