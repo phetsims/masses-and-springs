72b0626f07accd2c9c84752c123e101059cde3d1
phet-dev <phetadmin@gmail.com>
2022-05-24 03:45:12 -0600

    update copyright dates from daily grunt work

--- js/common/view/ReferenceLineNode.js
+++ js/common/view/ReferenceLineNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Responsible for the attributes associated with the reference line nodes.
