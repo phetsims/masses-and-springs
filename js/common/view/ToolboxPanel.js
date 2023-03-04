132333084a19e73296c24eaf085fcad467a1ebfb
phet-dev <phetadmin@gmail.com>
2023-03-04 04:26:57 -0700

    update copyright dates from daily grunt work

--- js/common/view/ToolboxPanel.js
+++ js/common/view/ToolboxPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * Responsible for the toolbox panel and the ruler/timer icons held within
