9308f91db6416cf35f516d09498128934d50bfea
phet-dev <phetadmin@gmail.com>
2023-09-02 03:16:38 -0600

    update copyright dates from daily grunt work

--- js/intro/view/ConstantsControlPanel.js
+++ js/intro/view/ConstantsControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * Panel responsible for keeping the spring constant and spring thickness constant.
