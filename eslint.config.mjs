8744a44a103cfbd86008d884daaa9daccf44256e
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:44 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for masses-and-springs.
+ * ESLint configuration for masses-and-springs.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
